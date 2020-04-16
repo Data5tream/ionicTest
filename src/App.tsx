e6b8ff45afa6dcaabbb55b4f1bd6c680dd3ab625
Simon Stefan Barth <data5tream@protonmail.com>
2020-04-16 11:44:38 +0200

    Redirect all 404s to main store

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App: React.FC<AppProps> = ({ forceDarkmode }) => {
         <IonRouterOutlet id="main" animated={false}>
           <Route path="/store/:id" component={Stores} />
           <Route path="/configuration" component={Config} exact={true} />
-          <Route path="/" render={() => <Redirect to="/store/0" />} exact={true} />
+          <Route path="/" render={() => <Redirect to="/store/0" />} />
         </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
