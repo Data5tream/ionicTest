fe13f4ceb109c4d0bdf4fc83cb905ffac63d2f56
Simon Stefan Barth <data5tream@protonmail.com>
2020-04-16 13:11:45 +0200

    Move add store modal into own component

--- src/pages/Config.tsx
+++ src/pages/Config.tsx
@@ -5,32 +5,25 @@ import { connect } from 'react-redux';
 import {
   IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem,
   IonLabel, IonToggle, IonItemDivider, IonReorderGroup, IonReorder, IonFab,
-  IonFabButton, IonIcon, IonModal, IonInput, IonCard, IonCardHeader, IonCardTitle,
-  IonCardContent, IonButton, IonGrid, IonRow, IonCol, IonMenuButton,
+  IonFabButton, IonIcon, IonMenuButton,
 } from '@ionic/react';
 import { ItemReorderEventDetail } from '@ionic/core';
 import { addOutline } from 'ionicons/icons';
+
+import StoreModal from '../components/StoreModal';
 
 interface ConfigProps {
   forceDarkmode: boolean;
   stores: Array<{name: string;}>;
   toogleForceDarkmode: Function;
-  addNewStore: Function;
   changeStoreOrder: Function;
 }
 
-const Config: React.FC<ConfigProps> = ({ forceDarkmode, stores, toogleForceDarkmode, addNewStore, changeStoreOrder }) => {
+const Config: React.FC<ConfigProps> = ({ forceDarkmode, stores, toogleForceDarkmode, changeStoreOrder }) => {
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [storeName, setStoreName] = useState<string>('');
 
   const toggleDarkmode: Function = () => {
     toogleForceDarkmode(!forceDarkmode);
-  };
-
-  const saveNewStore: Function = () => {
-    setShowModal(false);
-    addNewStore(storeName);
-    setStoreName('');
   };
 
   const onReorder: Function = (event: CustomEvent<ItemReorderEventDetail>) => {
@@ -68,26 +61,7 @@ const Config: React.FC<ConfigProps> = ({ forceDarkmode, stores, toogleForceDarkm
             <IonIcon icon={addOutline} />
           </IonFabButton>
         </IonFab>
-        <IonModal isOpen={showModal}>
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Add a new store</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <IonInput value={storeName} placeholder="Store name" onIonChange={e => setStoreName(e.detail.value!)} />
-              <IonGrid>
-                <IonRow class="ion-justify-content-between">
-                  <IonCol>
-                    <IonButton color="danger" fill="outline" onClick={() => setShowModal(false)}>Close</IonButton>
-                  </IonCol>
-                  <IonCol class="ion-text-end">
-                    <IonButton color="success" onClick={() => saveNewStore()}>Save</IonButton>
-                  </IonCol>
-                </IonRow>
-              </IonGrid>
-            </IonCardContent>
-          </IonCard>
-        </IonModal>
+        <StoreModal showModal={showModal} setShowModal={setShowModal} />
       </IonContent>
     </IonPage>
   );
@@ -96,7 +70,6 @@ const Config: React.FC<ConfigProps> = ({ forceDarkmode, stores, toogleForceDarkm
 const mapStateToProps: any = (state: any) => state;
 const mapDispatchToProps: any = (dispatch: any) => ({
   toogleForceDarkmode: (value: boolean) => dispatch({ type: 'SET_FORCE_DARKMODE', value }),
-  addNewStore: (value: string) => dispatch({ type: 'ADD_NEW_STORE', value }),
   changeStoreOrder: (value: Array<number>) => dispatch({ type: 'CHANGE_STORE_ORDER', value }),
 });
 
