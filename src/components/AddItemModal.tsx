29b02562a97fd8dab46463184df66e3b43d8f17a
Simon Stefan Barth <data5tream@protonmail.com>
2020-04-18 15:54:42 +0200

    improve modals

--- src/components/AddItemModal.tsx
+++ src/components/AddItemModal.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 
 import {
-  IonModal, IonInput, IonCard, IonCardHeader, IonCardTitle,
-  IonCardContent, IonButton, IonGrid, IonRow, IonCol,
+  IonModal, IonInput, IonButton, IonGrid, IonRow, IonCol, IonHeader, IonTitle, IonContent, IonToolbar,
 } from '@ionic/react';
 
 interface RootState {
@@ -22,25 +21,29 @@ const AddItemModal: React.FC<RootState> = ({ showModal, setShowModal, storeId, a
   };
 
   return (
-    <IonModal isOpen={showModal}>
-      <IonCard>
-        <IonCardHeader>
-          <IonCardTitle>Add a new item</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>
-          <IonInput value={itemName} placeholder="Item name..." onIonChange={(e): void => setItemName(e.detail.value!)} />
-          <IonGrid>
-            <IonRow class="ion-justify-content-between">
-              <IonCol>
-                <IonButton color="danger" fill="outline" onClick={(): void => setShowModal(false)}>Close</IonButton>
-              </IonCol>
-              <IonCol class="ion-text-end">
-                <IonButton color="success" onClick={(): void => saveNewItem()}>Save</IonButton>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-        </IonCardContent>
-      </IonCard>
+    <IonModal isOpen={showModal} onWillDismiss={() => setShowModal(false)}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Add a new item</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonInput value={itemName} placeholder="Item name..." onIonChange={(e): void => setItemName(e.detail.value!)} />
+            </IonCol>
+          </IonRow>
+          <IonRow class="ion-justify-content-between">
+            <IonCol>
+              <IonButton color="danger" fill="outline" onClick={(): void => setShowModal(false)}>Close</IonButton>
+            </IonCol>
+            <IonCol class="ion-text-end">
+              <IonButton color="success" onClick={(): void => saveNewItem()}>Save</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </IonModal>
   );
 };
--- src/components/AddStoreModal.tsx
+++ src/components/AddStoreModal.tsx
@@ -3,8 +3,7 @@ import React, { useState } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 
 import {
-  IonModal, IonInput, IonCard, IonCardHeader, IonCardTitle,
-  IonCardContent, IonButton, IonGrid, IonRow, IonCol,
+  IonModal, IonInput, IonButton, IonGrid, IonRow, IonCol, IonHeader, IonToolbar, IonTitle, IonContent,
 } from '@ionic/react';
 
 interface RootState {
@@ -36,25 +35,25 @@ const AddStoreModal: React.FC<Props> = (props: Props) => {
   };
 
   return (
-    <IonModal isOpen={props.showModal}>
-      <IonCard>
-        <IonCardHeader>
-          <IonCardTitle>Add a new store</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>
-          <IonInput value={storeName} placeholder="Store name" onIonChange={(e): void => setStoreName(e.detail.value!)} />
-          <IonGrid>
-            <IonRow class="ion-justify-content-between">
-              <IonCol>
-                <IonButton color="danger" fill="outline" onClick={(): void => props.setShowModal(false)}>Close</IonButton>
-              </IonCol>
-              <IonCol class="ion-text-end">
-                <IonButton color="success" onClick={(): void => saveNewStore()}>Save</IonButton>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-        </IonCardContent>
-      </IonCard>
+    <IonModal isOpen={props.showModal} onWillDismiss={() => props.setShowModal(false)}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Add a new store</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonInput value={storeName} placeholder="Store name" onIonChange={(e): void => setStoreName(e.detail.value!)} />
+        <IonGrid>
+          <IonRow class="ion-justify-content-between">
+            <IonCol>
+              <IonButton color="danger" fill="outline" onClick={(): void => props.setShowModal(false)}>Close</IonButton>
+            </IonCol>
+            <IonCol class="ion-text-end">
+              <IonButton color="success" onClick={(): void => saveNewStore()}>Save</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </IonModal>
   );
 };
